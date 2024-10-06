2c640ac4d93ff4782bb357434c88cb1b593b988d
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-05 21:41:53 -0400

    Create recursive function to calculate department salary (+ changing data structure)

--- department_salary.js
+++ department_salary.js
@@ -1,61 +1,75 @@
 // Task 1: Create a Department Structure
 
 const company = {
-    departments: [
-        {
-            departmentName: 'TradingCards',
-            employees: [
-                {
-                    name: 'Lazareth',
-                    salary: 23000,
-                    subordinates: [
-                        {
-                            name: 'Kenneth',
-                            salary: 12575,
-                            subordinates: []
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            deparmentName: 'BoardGames',
-            employees: [
-                {
-                    name: 'Madam Kirlia',
-                    salary: 84200,
-                    subordinates: [
-                        {
-                            name: 'Serena',
-                            salary: 39820,
-                            subordinates: [
-                                {
-                                    name: 'Cynthia',
-                                    salary: 12575,
-                                    subordinates: []
-                                }
-                            ]                    
-                        }
-                    ]
-                },
-                {
-                    name: 'Lance',
-                    salary: 45040,
-                    subordinates: []
-                }
-            ]
-         },
-         {
-            departmentName: 'CustomerSupport',
-            employees: [
-                {
-                    name: 'Unnamed Intern',
-                    salary: 4310,
-                    subordinates: []
-                }
-            ]
-         }    
-    ]
-};
-
+        departments: [
+            {
+                departmentName: 'TradingCards',
+                employees: [
+                    {
+                        name: 'Leon',
+                        salary: 151000,
+                        subordinates: [
+                            {
+                             name: 'Steven',
+                                salary: 10376,
+                                subordinates: [
+                                    {
+                                        name: 'Calem',
+                                        salary: 58888,
+                                        subordinates: []
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Iris',
+                        salary: 57075,
+                        subordinates: []
+                    }
+                ]
+            },
+            {
+                departmentName: 'BoardGames',
+                employees: [
+                    {
+                        name: 'Serena',
+                        salary: 85000,
+                        subordinates: [
+                            {
+                                name: 'Cynthia',
+                                salary: 70000,
+                                subordinates: []
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Lance',
+                        salary: 95000,
+                        subordinates: []
+                    }
+                ]
+            }
+        ]
+    };
 console.log (company)
+
+// Task 2: Create a Recursive Function to Calculate Total Salary for a Department
+
+function calculateDepartmentSalary (department) {
+// initializing total salary
+  let totalSalary = 0;
+// initial calculation
+    for (let employee of department) {
+       totalSalary += employee.salary;
+
+       // using recursion here to include calculation of subordinate salary
+        if (employee.subordinates.length > 0) {
+            totalSalary += calculateDepartmentSalary (employee.subordinates)
+        }
+    }
+return totalSalary;
+}
+const TradingCardSalary = calculateDepartmentSalary(company.departments[0].employees)
+console.log(`Total Sales for the Trading Card Department: $${TradingCardSalary}`)
+// Desired Output: $277339
