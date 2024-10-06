854f9a03118a3e7719ecd3f06bb0c0b31e74ad4b
Kenzo Cogswell <kcogswell@usf.edu>
2024-10-05 22:02:36 -0400

    Calculate a Function to Calculate the Total Salary for All Departments

--- department_salary.js
+++ department_salary.js
@@ -73,3 +73,18 @@ return totalSalary;
 const TradingCardSalary = calculateDepartmentSalary(company.departments[0].employees)
 console.log(`Total Sales for the Trading Card Department: $${TradingCardSalary}`)
 // Desired Output: $277339
+
+// Task 3: Create a Function to Calculate the Total Salary for all departments
+
+function calculateCompanySalary (company) {
+// initializing company salary
+    let companySalary = 0
+// calling back the previous department calculation function, now using it for each department
+    for (let department of company.departments) {
+        companySalary += calculateDepartmentSalary(department.employees);
+    };
+return companySalary;
+}
+
+console.log(`Total Company Sales: $${calculateCompanySalary(company)}`)
+// Desired Output: $527339
